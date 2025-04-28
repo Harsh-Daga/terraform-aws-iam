c1514383c13fb4187e74835f6aed2cde0b4f034b
Harsh <64948227+Harsh-Daga@users.noreply.github.com>
2025-04-28 16:00:56 +0530

    fix

--- modules/iam-role-for-service-accounts-eks/main.tf
+++ modules/iam-role-for-service-accounts-eks/main.tf
@@ -1,12 +1,11 @@
 data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
 
 locals {
   account_id          = data.aws_caller_identity.current.account_id
   partition           = data.aws_partition.current.partition
   dns_suffix          = data.aws_partition.current.dns_suffix
-  region              = data.aws_region.current.name
+  region              = "mx-central-1"
   role_name_condition = var.role_name != null ? var.role_name : "${var.role_name_prefix}*"
 }
 
